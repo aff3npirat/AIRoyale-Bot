e95e72a5daf81d63a1d29ca686831e0d8d699a96
aff3npirat <jurek.elliesen@web.de>
2023-02-17 17:36:33 +0100

    Add pbatch side detector

--- state/units.py
+++ state/units.py
@@ -1,4 +1,8 @@
+import numpy as np
+from PIL import Image
+
 from state.onnx_detector import OnnxDetector
+from constants import SIDE_H, SIDE_W, UNIT_H, UNIT_W
 
 
 
@@ -8,9 +12,21 @@ class UnitDetector(OnnxDetector):
         super().__init__(model_path)
 
     def _preprocess(self, img):
+        img = img.resize((UNIT_H, UNIT_W), Image.BICUBIC)
+        img = np.array(img, dtype=np.float32)
+        img = img.transpose(2, 0, 1)
+        img = np.expand_dims(img, axis=0)
         return img / 255
 
     def run(self, img, conf_thres=0.725, iou_thres=0.5):
+        """
+        Returns 0 or 1, 0 if unit present on image is in blue team, 1 otherwise.
+
+        Parameters
+        ----------
+        img : PIL.Image
+            A PIL image with channels as last dimension. Pixel values should range from 0 to 255.
+        """
         img = self._preprocess(img)
 
         pred = self.sess.run([self.output_name], {self.input_name: img})[0]
@@ -26,14 +42,24 @@ class SideDetector(OnnxDetector):
         super().__init__(model_path)
 
     def _preprocess(self, img):
+        img = img.resize((SIDE_H, SIDE_W), Image.BICUBIC)
+        img = np.array(img, dtype=np.float32)
+        img = np.expand_dims(img, axis=0)
         return img / 255
 
     def run(self, img):
         """
-        Returns 0 or 1, 1 if unit present on image is in blue team, 0 otherwise.
-        img: array with shape (M, SIDE_W, SIDE_H)
-        """
-        pred = self.sess.run([self.output_name], {self.input_name: img})
+        Returns 0 or 1, 0 if unit present on image is in blue team, 1 otherwise.
 
-        return pred
+        Parameters
+        ----------
+        img : PIL.Image
+            A PIL image with channels as last dimension. Pixel values should range from 0 to 255.
+        """
+        img = self._preprocess(img)
+
+        pred = self.sess.run([self.output_name], {self.input_name: img})[0]  # shape (1, 2)
+        team = np.argmax(pred)
+
+        return team
         
