f92bfd21223f2b6d6c6e1018cd819ea510d2b4a1
aff3npirat <jurek.elliesen@web.de>
2023-02-17 16:44:58 +0100

    Add basic side detector setup

--- state/units.py
+++ state/units.py
@@ -15,10 +15,25 @@ class UnitDetector(OnnxDetector):
 
         pred = self.sess.run([self.output_name], {self.input_name: img})[0]
 
-        bboxes = self.nms(pred, conf_thres=conf_thres, iou_thres=iou_thres)
+        bboxes = self.nms(pred, conf_thres=conf_thres, iou_thres=iou_thres)  # shape (M, 6)
 
         return bboxes
 
 
-class SideDetector:
-    pass
+class SideDetector(OnnxDetector):
+
+    def __init__(self, model_path):
+        super().__init__(model_path)
+
+    def _preprocess(self, img):
+        return img / 255
+
+    def run(self, img):
+        """
+        Returns 0 or 1, 1 if unit present on image is in blue team, 0 otherwise.
+        img: array with shape (M, SIDE_W, SIDE_H)
+        """
+        pred = self.sess.run([self.output_name], {self.input_name: img})
+
+        return pred
+        
