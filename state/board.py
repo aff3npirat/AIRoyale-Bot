8f8ae6656f6714bbbe1fa509f4cc196e9492fdba
aff3npirat <jurek.elliesen@web.de>
2023-02-28 13:01:28 +0100

    Add board embedding network

--- state/board.py
+++ state/board.py
@@ -3,8 +3,20 @@ import torch.nn as nn
 
 
 
-class IBasicBlock(nn.Module):
-    pass
+class ResBlk(nn.Module):
+    
+    def __init__(self):
+        self.conv1 = nn.Conv2d(16, 16, (3, 3), padding=1, stride=1, bias=True)
+        self.prelu = nn.PReLU(16)
+        self.conv2 = nn.Conv2d(16, 16, (3, 3), padding=1, stride=1, bias=True)
+
+    def forward(self, x):
+        identity = x
+        x = self.conv1(x)
+        x = self.prelu(x)
+        x = self.conv2(x)
+        x = x + identity
+        return x
 
 
 class BoardEmbedding(nn.Module):
@@ -13,8 +25,14 @@ class BoardEmbedding(nn.Module):
         super().__init__()
 
         self.blocks = nn.Sequential(
-            *[IBasicBlock() for _ in range(5)]
+            *[ResBlk() for _ in range(5)]
         )
+        self.downsample = nn.Conv2d(16, 16, (3, 3), padding=0, stride=2, bias=False)
+        self.linear = nn.Linear(14*8*16, 512, bias=False)
 
     def forward(self, x):
-        return self.blocks(x)
+        x = self.blocks(x)
+        x = self.downsample(x)
+        x = x.flatten(1)
+        x = self.linear(x)
+        return x
