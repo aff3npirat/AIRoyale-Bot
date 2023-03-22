3492938396dcec3d2534391b5ddd5a092ef618d0
aff3npirat <jurek.elliesen@web.de>
2023-03-22 10:49:05 +0100

    Add method to summarize time log

--- timing.py
+++ timing.py
@@ -1,6 +1,10 @@
+import os
 import timeit
 import inspect
+from argparse import ArgumentParser
 from functools import wraps
+
+import matplotlib.pyplot as plt
 
 
 
@@ -24,3 +28,37 @@ def exec_time(func):
         logger.info(f"{name}: {toc - tic}")
         return result
     return wrapper
+
+
+def summarize_log(log_file, output):
+    os.makedirs(output, exist_ok=True)
+
+    with open(log_file, "rt") as f:
+        lines = f.readlines()[1:]
+
+    funcs = {}
+    for line in lines:
+        name, seconds = line.rstrip("\n").split(": ")
+
+        if name not in funcs:
+            funcs[name] = []
+
+        funcs[name].append(float(seconds))
+
+    for name, seconds in funcs.items():
+        mean = sum(seconds)/len(seconds)
+        plt.title(f"{name} | mean={mean:.4E}")
+        plt.plot(seconds, "--", color="orange")
+        plt.axhline(mean, "-", color="blue")
+        plt.tight_layout()
+        plt.savefig(os.path.join(output, f"{name}.png"))
+        plt.close("all")
+
+
+if __name__ == "__main__":
+    parser = ArgumentParser()
+    parser.add_argument("in-file", type=str)
+    parser.add_argument("output", type=str)
+    
+    args = parser.parse_args()
+    summarize_log(args.in_file, args.output)
