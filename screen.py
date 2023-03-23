2efa9d72b3e83287912f09151a4bc8c8e5e4f468
aff3npirat <jurek.elliesen@web.de>
2023-03-23 16:14:31 +0100

    Refactoring

--- screen.py
+++ screen.py
@@ -14,7 +14,7 @@ class Screen:
         self.adb_exec = adb_path
         self.target = f"localhost:{port}"
         self.port = port
-        window_size = subprocess.check_output([self.adb_exec, '-s', self.target, 'shell', 'wm', 'size'])
+        window_size = subprocess.check_output(f"{self.adb_exec} -s {self.target} shell wm size")
         window_size = window_size.decode('ascii').replace('Physical size: ', '')
         self.width, self.height = [int(i) for i in window_size.split('x')]
 
@@ -26,7 +26,7 @@ class Screen:
         x = x / SCREENSHOT_WIDTH * self.width
         y = y / SCREENSHOT_HEIGHT * self.height
 
-        subprocess.run([self.adb_exec, '-s', self.target, 'shell', 'input', 'tap', str(x), str(y)])
+        subprocess.run(f"{self.adb_exec} -s {self.target} shell input tap {x} {y}")
 
     @exec_time
     def select_place_unit(self, slot_idx, side):
@@ -39,7 +39,7 @@ class Screen:
         """
         Take a screenshot of the emulator
         """
-        screenshot_bytes = subprocess.run([self.adb_exec, '-s', self.target, 'exec-out', 'screencap'], check=True, capture_output=True).stdout
+        screenshot_bytes = subprocess.run(f"{self.adb_exec} -s {self.target} exec-out screencap", check=True, capture_output=True).stdout
         screenshot = Image.frombuffer('RGBA', (self.width, self.height), screenshot_bytes[12:], 'raw', 'RGBX', 0, 1)
         screenshot = screenshot.convert('RGB').resize((SCREENSHOT_WIDTH, SCREENSHOT_HEIGHT), Image.BILINEAR)
         return screenshot
