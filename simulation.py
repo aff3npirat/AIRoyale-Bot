686fa825e40e981c58010bcc17feedde6a066dfe
aff3npirat <jurek.elliesen@web.de>
2023-07-25 14:48:30 +0200

    Finish update logic.

--- simulation.py
+++ simulation.py
@@ -39,7 +39,22 @@ class CRSim:
         self.blue_units = []  # (label, tile_x, tile_y)
         self.red_units = []
 
-        self.units = []  # label, x, y, health, action_time, attk_range, attk_speed, damage, sight_range
+        # positions of tile next to bridge, used as target for units moving towards bridge
+        self.left_path_tile_x = ...
+        self.blue_path_tile_y = ...
+        self.right_path_tile_x = ...
+        self.red_path_tile_y = ...
+
+        # height of king towers, used for units on/after bridge
+        self.red_king_y = ...
+        self.blue_king_y = ...
+
+        # x coord, spliting board into left and right. All units with x<=left_side_x are left side.
+        self.left_side_x = ...
+
+        self.blue_units = np.empty((0, 11))  # label, x, y, health, action_time, attk_range, attk_speed, damage, sight_range, x_coeff, movement_speed
+        self.red_units = np.empty((0, 11))
+
 
     def get_states(self):
         # TODO
@@ -47,56 +62,56 @@ class CRSim:
 
     def update(self, actions):
         # TODO move, attack, repeat
-        self.units[4] += self.delta_seconds
-
-        dists = np.abs(self.units[:, (1, 2), None] - self.units[:, (1, 2), None].T)
+        dists = np.abs(self.blue_units[:, (1, 2), None] - self.red_units[:, (1, 2), None].T)
         dists = np.sqrt(np.sum(dists**2, axis=1))
+        
+        # mask out any illegal targets (such as self, or flying)
+        # TODO
         np.fill_diagonal(dists, np.inf)
 
-        target_dist = np.min(dists, axis=1)
-        target_idx = np.argmin(dists, axis=1)
+        dists_blue = np.min(dists, axis=2)  # dist for each blue unit to closest red unit
+        targets_blue = np.argmin(dists, axis=2)
 
+        dists_red = np.min(dists, axis=1)
+        targets_red = np.argmin(dists, axsis=1)
+
+        n_blue = len(self.blue_units)
+        n_red = len(self.red_units)
+
+        units = np.concatenate((self.blue_units, self.red_units), axis=0)
+        targets_idx = np.concatenate((targets_blue, targets_red + n_blue), axis=0)
+        dists = np.concatenate((dists_blue, dists_red), axis=0)
+
+        units[:, 4] += self.delta_seconds
+        
         # attack unit
-        mask = (target_dist<=self.units[5])  # dist <= attk_range
-        if mask.any():
-            self.units[mask, 4] -= self.units[mask, 6]  # update action time
-            self.units[target_idx[mask], 3] -= self.units[mask, 7]  # update health
-        
+        mask_attack = (dists<=units[:, 5] & units[:, 4]>=0.0)
+        if mask_attack.any():
+            units[mask_attack, 4] -= units[mask_attack, 6]  # update action time
+            units[targets_idx[mask_attack], 3] -= units[mask_attack, 7]  # update health
 
+        # move units that did not attack
+        target_tiles = np.zeros((n_blue + n_red, 2))
 
+        for start, end, units_, y, king_y in (
+            (0, n_blue, self.blue_units, self.blue_path_tile_y, self.red_king_y),
+            (n_blue, n_blue+n_red, self.red_path_tile_y, self.blue_king_y),
+        ):
+            target_tiles[start:end, 1][units_[:, 2]>y] = y
+            target_tiles[start:end, 1][units_[:, 2]<=y] = king_y
+            target_tiles[start:end, 0][units_[:, 1]>self.left_side_x] = self.right_path_tile_x
+            target_tiles[start:end, 0][units_[:, 1]<=self.left_side_x] = self.left_path_tile_x
 
+        directions = target_tiles - units[:, (1, 2)]
+        # directions[:, 0] *= units[:, 9]
 
-def run(eps, blue_cards, red_cards, king_levels=None):
-    sim = CRSim(
-        blue_cards=blue_cards, red_cards=red_cards, king_levels=king_levels
-    )
+        # override for units that have a target
+        mask_sight = (dists<=units[8])
+        directions[mask_sight & ~mask_attack] = units[targets_idx[mask_sight & ~mask_attack], (1, 2)] - units[mask_sight & ~mask_attack, (1, 2)]
 
-    qnet = QNet([512+NEXT_CARD_END, 128, 64, 5], activation="sigmoid", bias=True, feature_extractor=False)
+        directions /= np.sqrt(np.sum(directions**2, axis=1))  # normalize to unit length
+        directions *= self.units[:, 10] * self.delta_seconds # get movement in one frame
 
+        units[:, (1, 2)] += directions
 
-    while not sim.game_over:
-        blue_state, red_state = sim.get_states()
-        
-        actions = {}
-        for state, team in zip((blue_state, red_state), ("blue", "red")):
-            illegal = SingleDeckBot.get_illegal_actions(state)
-
-            if random.random() < eps:
-                action = random.choice(list(range(5)))
-                if action in illegal:
-                    action = 0
-            else:
-                qvals = qnet(state)
-                qvals[illegal] = -torch.inf
-
-                action = torch.argmax(qvals)
-        
-            actions[team] = action
-
-        sim.update(actions)
-    
-
-    
-
-
-
+        self.blue_units, self.red_units = units[:n_blue], units[n_blue:]
